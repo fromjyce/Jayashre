7d9f8b7ac2c2d49022700b1b0e025326d040875f
jaya2 <jaya2004kra@gmail.com>
2023-12-24 07:46:00 +0530

    added about fragment

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import "../styles/About.css";
+import { motion } from "framer-motion";
+
+function About () {
+    return (<div className="About" id="About">
+        <div className="about-container">
+           <div className="about-title"><h1 className="title-about">About</h1></div>
+           <div className="rectangle"></div>
+        </div>
+    </div>);
+}
+
+export default About;
