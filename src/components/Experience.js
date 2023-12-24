991489947f20f8d85bc9c06da66bdca0fc4a2c27
jaya2 <jaya2004kra@gmail.com>
2023-12-24 23:58:04 +0530

    added seventh and eighth experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,6 @@
 import "../styles/Experience.css";
 import { FaPython, FaPenAlt, FaGithub, FaJava } from "react-icons/fa";
-import { SiTorbrowser } from "react-icons/si";
+import { SiTorbrowser, SiWebpack } from "react-icons/si";
 import { CgWebsite, CgBrowse } from "react-icons/cg";
 
 
@@ -231,7 +231,7 @@ function Experience () {
                 <br />
                 <div className="stack-concepts right-aligned-paragraph">
                   <strong>Tech Stack & Concepts: </strong>Java, API, OOPs,
-                  JavaFX-GUI,
+                  JavaFX-GUI
                 </div>
                 <p className="job-description right-aligned-paragraph">
                   Throughout my Java programming internship at CodSoft, I
@@ -247,6 +247,94 @@ function Experience () {
                   using external APIs, while the Student Management System
                   showcased my proficiency in designing user-friendly interfaces
                   for efficient data management.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="seventh-experience">
+            <div className="experience-point seventh">
+              <div className="e-logo">
+                <SiWebpack size={25} color="#343434" />
+              </div>
+            </div>
+            <div div className="experience-box-white-black seventh-exp">
+              <div className="full-experience-paragraph-left">
+                <div className="date left-aligned-paragraph">
+                  September 2023 - October 2023
+                </div>{" "}
+                <br />
+                <div className="job-title left-aligned-paragraph">
+                  Web Development Intern{" "}
+                  <a href="/#">
+                    <FaGithub size={22} color="#db2b39" />
+                  </a>{" "}
+                </div>
+                <br />
+                <div className="company left-aligned-paragraph">
+                  Info Aid Tech
+                </div>
+                <br />
+                <div className="stack-concepts left-aligned-paragraph">
+                  <strong>Tech Stack & Concepts: </strong>Web Development, HTML,
+                  CSS, JavaScript, Django, MongoDB, MySQL, ReactJS, Node JS,
+                  Flask, API, Figma
+                </div>
+                <p className="job-description left-aligned-paragraph">
+                  Throughout my immersive internship at InfoAidTech, I engaged
+                  in a series of impactful web development projects that
+                  underscored my growth as a developer. Starting with a
+                  captivating product landing page for premium watches, I honed
+                  my skills in crafting sleek and user-friendly interfaces. The
+                  development of a functional calculator then challenged me to
+                  handle a range of arithmetic operations and more complex
+                  calculations. Diving into practical utility, I created a
+                  temperature converter, streamlining the process of unit
+                  conversions. The culmination of my internship involved the
+                  creation of an enticing restaurant website, where I combined
+                  aesthetics with functionality.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="eighth-experience">
+            <div className="experience-point eighth">
+              <div className="e-logo">
+                <FaJava size={25} color="#343434" />
+              </div>
+            </div>
+            <div div className="experience-box-white-black eighth-exp">
+              <div className="full-experience-paragraph-right">
+                <div className="date right-aligned-paragraph">
+                  September 2023 - October 2023
+                </div>{" "}
+                <br />
+                <div className="job-title right-aligned-paragraph">
+                  <a href="/#">
+                    <FaGithub size={22} color="#db2b39" />
+                  </a>{" "}
+                  Java Programming Intern{" "}
+                </div>
+                <br />
+                <div className="company right-aligned-paragraph">
+                  Cipher Byte Technologies
+                </div>
+                <br />
+                <div className="stack-concepts right-aligned-paragraph">
+                  <strong>Tech Stack & Concepts: </strong>Java, API, OOPs,
+                  JavaFX-GUI,
+                </div>
+                <p className="job-description right-aligned-paragraph">
+                  During my Java programming internship at CipherByte, I evolved
+                  from an intern to a skilled Java programmer through a series
+                  of impactful projects. I created a "Guess Number" game
+                  incorporating features like limited attempts and scoring. The
+                  "ATM Interface" simulated a user-friendly banking experience
+                  with transaction handling. Developing an "Online Examination
+                  Tool," I implemented secure login, profile management, and
+                  real-time feedback for multiple-choice questions. Finally, the
+                  "Digital Library Management System" automated book-keeping,
+                  issuance, fine calculation, and report generation, enhancing
+                  library operations.
                 </p>
               </div>
             </div>
