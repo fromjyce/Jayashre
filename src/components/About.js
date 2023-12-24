86e7eb5c8ad3d0eb5b32a17430179c6fee6c14e8
jaya2 <jaya2004kra@gmail.com>
2023-12-24 09:21:53 +0530

    added about fragment

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,32 @@ import "../styles/About.css";
 import { motion } from "framer-motion";
 
 function About () {
-    return (<div className="About" id="About">
+    return (
+      <div className="About" id="About">
         <div className="about-container">
-           <div className="about-title"><h1 className="title-about">About</h1></div>
-           <div className="rectangle"></div>
+          <div className="about-title">
+            <h1 className="title-about">ABOUT</h1>
+          </div>
+          <div className="rectangle">
+            <div className="about-paragraph">
+              <p>
+                Hey there! &#128075; <br /> I'm a Cyber Security student with a
+                knack for both coding and cybersecurity &#128640; <br /> Beyond
+                the textbooks, I thrive on hands-on experiences, diving into
+                real-world challenges. <br /> I'm all about that continuous
+                learning vibe – always hungry for more knowledge. My goal? To
+                use my skills to come up with cool, innovative solutions. <br />
+                I'm totally open to collaborations, especially when it involves
+                strengthening security measures and taking on cyber risks.
+                <br /> Let's team up to make the digital world a safer place
+                &#128187; &#128274;
+              </p>
+            </div>
+          </div>
+          &emsp; <br /> &emsp; <br /> &emsp; <br /> &emsp; <pre><br/></pre>
         </div>
-    </div>);
+      </div>
+    );
 }
 
 export default About;
