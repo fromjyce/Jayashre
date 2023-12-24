6944b329f434adec00610150f3fcde2d59dfa0ba
jaya2 <jaya2004kra@gmail.com>
2023-12-24 07:45:03 +0530

    added about fragment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Home />
+      <About />
     </React.Fragment>
   );
 }
