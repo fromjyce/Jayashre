715e0cd45abd4f403c417b71d6bcf69a6a36d320
jaya2 <jaya2004kra@gmail.com>
2023-12-24 09:31:01 +0530

    added experience fragment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Experience from "./components/Experience";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Experience />
     </React.Fragment>
   );
 }
