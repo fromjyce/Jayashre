df4f18a83b9c87048450ebeb16ddcf2b67c313a1
jaya2 <jaya2004kra@gmail.com>
2023-12-25 16:16:57 +0530

    added education

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,11 +2,14 @@ import "../styles/Education.css";
 
 function Education() {
     return (
-        <div className="Education" id="Education">
-            <div className="education-container">
-                <h1>HELLO</h1>
-            </div>
+      <div className="Education" id="Education">
+        <div className="education-container">
+          <div className="education-title">
+            <h1 className="title-education">EDUCATION</h1>
+          </div>
+          
         </div>
+      </div>
     );
 }
 
