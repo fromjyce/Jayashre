6aa1ccce095b5ee9946fa38ec8f7bbea2be029ef
jaya2 <jaya2004kra@gmail.com>
2023-12-25 11:27:20 +0530

    added education fragment

---
+++ src/components/Education.js
@@ -0,0 +1,13 @@
+import "../styles/Education.css";
+
+function Education() {
+    return (
+        <div className="Education" id="Education">
+            <div className="education-container">
+                <h1>HELLO</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Education;
