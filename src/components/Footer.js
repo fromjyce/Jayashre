c5d10218c13f407b91121a825c2d6e644f2c4aae
jaya2 <jaya2004kra@gmail.com>
2024-01-22 21:56:25 +0530

    added footer file

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "../styles/Footer.css";
+
+function Footer() {
+    return (
+      <div className="Footer" id="Footer">
+        <div className="footer-container">
+          </div>
+      </div>
+    );
+}
+
+export default Footer;
