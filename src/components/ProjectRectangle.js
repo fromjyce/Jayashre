44773a8d07c587ec95f7576e659fdcd4cb0ea86f
jaya2 <jaya2004kra@gmail.com>
2024-01-22 12:42:10 +0530

    added rectangle

--- src/components/ProjectRectangle.js
+++ src/components/ProjectRectangle.js
@@ -7,22 +7,25 @@ const ProjectRectangle = ({
   projectLink,
   projectContributor,
   projecttimeline,
-  projectNumber,
+  projectstack,
 }) => {
   return (
-    <div className="project-rectangle" data-number={projectNumber}>
+    <div className="project-rectangle">
       <h3 className="contributor-name">{projectContributor}</h3>
       <h2 className="project-title-name">{projectTitle}</h2>
       <h3 className="timeline">{projecttimeline}</h3>
       <p className="project-description">{projectDescription}</p>
-
+      <h3 className="project-stack">
+        <strong>Tech Stack & Skills: </strong>
+        {projectstack}
+      </h3>
       <a
         className="project-github-icon"
         href={projectLink}
         target="_blank"
         rel="noopener noreferrer"
       >
-          <FaSquareGithub size={27} color="#db2b39" />
+        <FaSquareGithub size={27} color="#db2b39" />
       </a>
     </div>
   );
