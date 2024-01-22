8887cf16a967856e7a41a10bc06141e8aecdc6ff
jaya2 <jaya2004kra@gmail.com>
2024-01-22 20:26:11 +0530

    added achievements fragment js file

---
+++ src/components/Achievements.js
@@ -0,0 +1,15 @@
+import "../styles/Achievements.css";
+
+function Achievements() {
+    return (
+      <div className="Achievements" id="Achievements">
+        <div className="achievements-container">
+          <div className="achievements-title">
+            <h1 className="title-achievements">ACHIEVEMENTS</h1>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Achievements;
