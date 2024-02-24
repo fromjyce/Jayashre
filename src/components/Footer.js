02b791f7ddc4a21db7ef06e67abd503c7c186476
jaya2 <jaya2004kra@gmail.com>
2024-02-24 22:09:35 +0530

    added footer files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,25 @@ function Footer() {
     return (
       <div className="Footer" id="Footer">
         <div className="footer-container">
+          <div className="boxes-container">
+            <div className="box-item one">
+              <div className="first-item first-line">
+                <h1 className="logo-title first-half">J</h1>
+                <h1 className="logo-title second-half">S</h1>&emsp;
+                <h1 className="name-title first-half">Jaya</h1>
+                <h1 className="name-title second-half">shre</h1>
+              </div>
+              <div className="first-item second-line">
+                As a budding cybersecurity enthusiast with coding skills, I'm
+                excited about exploring and growing in this ever-evolving
+                field.
+              </div>
+              <div className="first-item third-line"></div>
+            </div>
+            <div className="box-item two"></div>
+            <div className="box-item three"></div>
           </div>
+        </div>
       </div>
     );
 }
