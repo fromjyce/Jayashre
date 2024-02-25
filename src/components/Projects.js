eeebc64965b03b33658e2635e7facf36670b6f7d
jaya2 <jaya2004kra@gmail.com>
2024-02-25 11:32:32 +0530

    added project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
       title: "Emotions based Music Player",
       description:
         "A first-semester mini project's C program played songs based on user-input emotions using a simple interface and external libraries.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/SongPlayer-In-C",
       contributor: "Solo Contributor",
       timeline: "October 2022 - December 2022",
       stack: "C, Terminal Emulation",
@@ -18,7 +18,7 @@ const Projects = () => {
       title: "Smile Snap Wave Stop",
       description:
         "The project involves smile detection and screenshot capture using OpenCV, along with hand detection to terminate the program.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/SmileSnapWaveStop",
       contributor: "Solo Contributor",
       timeline: "May 2023 - June 2023",
       stack: "Python, OpenCV, Tkinter, Mediapipe, Haarcascade",
@@ -27,7 +27,7 @@ const Projects = () => {
       title: "Barcode Based Attendance System",
       description:
         "Developed a Python-based attendance system using barcode scanning (tkinter GUI, pyzbar, SVM Classifier, OpenCV) and Excel files for user-friendly database management.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/BarCodeAttendanceSystem",
       contributor: "Solo Contributor",
       timeline: "March 2023 - May 2023",
       stack: "Python, Tkinter, Excel, OpenCV, Pyzbar, OpenPyxl",
@@ -36,7 +36,7 @@ const Projects = () => {
       title: "American Sign Language Detection",
       description:
         "An ASL Gesture Recognition System in Python using CNNs for Improved Accessibility and Inclusivity.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/AmericanSignLanguageDetection",
       contributor: "Team Contributor",
       timeline: "March 2023 - May 2023",
       stack: "Python, Tkinter, Tensorflow-Keras, OpenCV",
@@ -45,7 +45,7 @@ const Projects = () => {
       title: "CameraX",
       description:
         "An innovative camera app coded in Kotlin, leveraging the power of CameraX library for a seamless photography experience",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/CameraX",
       contributor: "Solo Contributor",
       timeline: "July 2023 - August 2023",
       stack: "Kotlin, Android Studio, CameraX, Android",
@@ -54,7 +54,7 @@ const Projects = () => {
       title: "A Basic To-do Web App",
       description:
         "Built a minimalist to-do web app with HTML, CSS, and potentially JavaScript, enabling users to efficiently manage tasks, including features for editing, marking completion, and date/time tracking.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/OasisInfoBytePrograms/tree/main/LevelTwo/TaskThree",
       contributor: "Solo Contributor",
       timeline: "August 2023 - September 2023",
       stack: "HTML, Figma, Javascript, CSS, UX",
@@ -63,7 +63,7 @@ const Projects = () => {
       title: "Real Time Currency Converter",
       description:
         "Crafted a currency converter using Apilayer API, allowing base-to-target currency selection, real-time exchange rates fetching, accurate conversions, and clear result display with target currency symbols.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/CodSoftPrograms/blob/main/CurrencyConverter.java",
       contributor: "Solo Contributor",
       timeline: "August 2023 - September 2023",
       stack: "Java, Terminal Emulation, API Integration",
@@ -73,7 +73,7 @@ const Projects = () => {
       title: "Student Management System",
       description:
         "Utilized JavaFX for a user-friendly interface, implementing a Student class with attributes to facilitate tasks like adding, editing, and displaying student details, ensuring accurate input validation and information integrity through data storage mechanisms.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/CodSoftPrograms/blob/main/StudentManagementSystem.java",
       contributor: "Solo Contributor",
       timeline: "August 2023 - September 2023",
       stack: "Java, SwingFX, UX, GUI",
@@ -82,7 +82,7 @@ const Projects = () => {
       title: "OpenChain",
       description:
         "A Solidity smart contract transforming the digital content marketplace, was developed using ThirdWeb",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/OpenChain",
       contributor: "Solo Contributor",
       timeline: "October 2023 - November 2023",
       stack: "Solidity, Remix IDE, Thirdweb, Blockchain, Ethereum",
@@ -91,7 +91,7 @@ const Projects = () => {
       title: "AirPic",
       description:
         "A collaborative project revolutionizing camera interaction. Introduced smile detection, hand gesture-controlled capture, zoom via gestures, and dynamic filters",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/AirPic",
       contributor: "Team Contributor",
       timeline: "June 2023 - November 2023",
       stack:
@@ -101,7 +101,7 @@ const Projects = () => {
       title: "PenGUIn",
       description:
         "Developed a user-friendly Linux app for OS hardening with essential and advanced security options, one-click USB and Tor exit node blocking, Essential and Advanced modes, and additional features like cache cleaning for heightened system security.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/PenGUIn",
       contributor: "Team Contributor",
       timeline: "October 2023 - December 2023",
       stack: "Shell Scripting, Operating System, PyGTK, OS Hardening",
@@ -110,7 +110,7 @@ const Projects = () => {
       title: "Deepfake Detection with InceptionResNetV2-BiLSTM",
       description:
         "This project enhances deepfake detection by combining InceptionResNetV2 and BiLSTM networks, addressing concerns about deceptive content in multimedia.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/DeepfakeDetection",
       contributor: "Solo Contributor",
       timeline: "October 2023 - December 2023",
       stack: "Python, Deep Learning, ",
@@ -119,35 +119,35 @@ const Projects = () => {
       title: "HireMeHorizon: Smart Campus Recruiter",
       description:
         "Applying machine learning to campus placement data involves analyzing student info, using models, and developing a website for undergraduates to assess placement and predict salaries.",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/HireMeHorizon",
       contributor: "Team Contributor",
       timeline: "October 2023 - December 2023",
       stack:
         "Python, HTML, CSS, Javascript, Flask, MySQL, React, Figma, Machine Learning, UX",
     },
     {
+      title: "Algorithmic Trading Model Development",
+      description:
+        "Development of an Algorithmic Trading Model for the BTC/USDT Crypto Market",
+      link: "https://github.com/fromjyce/Algorithmic-Trading",
+      contributor: "Team Contributor",
+      timeline: "January 2024" /*January */,
+      stack: "Python, Machine Learning, Trading",
+    },
+    {
       title: "CipherGuard: A Secure Encryption/Decryption Tool",
       description:
         "SwingFX Java application for text, document, audio, video, and image encryption",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/CipherGuard",
       contributor: "Solo Contributor",
       timeline: "Ongoing" /*December */,
       stack: "Java, SwingFX, GUI, Cryptography",
     },
     {
-      title: "Algorithmic Trading Model Development",
-      description:
-        "Development of an Algorithmic Trading Model for the BTC/USDT Crypto Market",
-      link: "https://example.com/project1",
-      contributor: "Team Contributor",
-      timeline: "Ongoing" /*January */,
-      stack: "Python, Machine Learning, Trading",
-    },
-    {
       title: "Personal Portfolio Website",
       description:
         "Designed a dynamic personal portfolio website showcasing my professional achievements, skills, experiences, achievements, and notable projects",
-      link: "https://example.com/project1",
+      link: "https://github.com/fromjyce/Jayashre",
       contributor: "Solo Contributor",
       timeline: "Ongoing" /*January */,
       stack: "React JS, HTML, CSS, Javascript, Figma, UX",
