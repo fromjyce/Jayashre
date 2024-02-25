1080e43ad250bca9e3bd420fb8f6e3822a2bea67
jaya2 <jaya2004kra@gmail.com>
2024-02-25 07:08:55 +0530

    added third line in first box

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,8 @@
 import "../styles/Footer.css";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+import { FaDiscord, FaLinkedin } from "react-icons/fa6";
 
 function Footer() {
     return (
@@ -9,16 +13,41 @@ function Footer() {
               <div className="first-item first-line">
                 <h1 className="logo-title first-half">J</h1>
                 <h1 className="logo-title second-half">S</h1>&emsp;
-                <h1 className="name-title first-half">Jaya</h1>
-                <h1 className="name-title second-half">shre</h1>
+                <h1 className="name-title first-half">JY</h1>
+                <h1 className="name-title second-half">CE</h1>
               </div>
               <div className="first-item second-line">
                 As a budding cybersecurity enthusiast with coding skills, I'm
-                excited about exploring and growing in this ever-evolving
-                field.
+                excited about exploring and growing in this ever-evolving field.
               </div>
               <div className="first-item third-line">
-                
+                <div className="contact-items-list" id="Contact">
+                  <Tippy
+                    content="Discord"
+                    className="footer-tooltip"
+                    animation="scale"
+                    arrow={false}
+                    hideOnClick={true}
+                    placement="bottom"
+                  >
+                    <a href="/#">
+                      <FaDiscord size={30} />
+                    </a>
+                  </Tippy>
+                  &nbsp; &nbsp; &nbsp;
+                  <Tippy
+                    content="LinkedIn"
+                    className="footer-tooltip"
+                    animation="scale"
+                    arrow={false}
+                    hideOnClick={true}
+                    placement="bottom"
+                  >
+                    <a href="/#">
+                      <FaLinkedin size={30} />
+                    </a>
+                  </Tippy>
+                </div>
               </div>
             </div>
             <div className="box-item two"></div>
