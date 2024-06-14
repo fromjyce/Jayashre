e30497d874ac70d2ce7fc8437213742e791d3456
jaya2 <jaya2004kra@gmail.com>
2024-06-14 15:22:19 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <div style={{ overflow: "hidden" }}>
+        <Home />
+      </div>
     </React.Fragment>
   );
 }
