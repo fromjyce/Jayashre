465f660452838cfdf45e5556538d7713af7afb57
jaya2 <jaya2004kra@gmail.com>
2024-06-14 12:15:02 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,120 +1,107 @@
-import { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { FaCircleInfo, FaPhone } from "react-icons/fa6";
-import { BsSuitcaseLgFill } from "react-icons/bs";
-import { TbPasswordUser } from "react-icons/tb";
-import { GoProjectRoadmap } from "react-icons/go";
-import { RiGraduationCapFill } from "react-icons/ri";
-import { PiMedalFill } from "react-icons/pi";
-import Tippy  from "@tippyjs/react";
-import "tippy.js/dist/tippy.css";
-import "tippy.js/animations/scale.css";
 import "../styles/Navbar.css";
+import { Link } from "react-scroll";
+import "remixicon/fonts/remixicon.css";
 
 function Navbar() {
-  const navRef = useRef();
 
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
+  const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+
+  /* Menu show */
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
+    });
+  }
+
+  /* Menu hidden */
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
+
+  const navLink = document.querySelectorAll(".nav__link");
+
+  const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
   };
+  navLink.forEach((n) => n.addEventListener("click", linkAction));
 
   return (
-    <header>
-      <a href="/#" className="logo">
-        <h3>JYCE.</h3>
-      </a>
-      <nav ref={navRef}>
-        <Tippy
-          content="About"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
+    <header className="navbar" id="header">
+      <nav className="nav container">
+        <Link
+          to="#"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="nav__logo"
         >
-          <a href="#about">
-            <FaCircleInfo />
-            <div className="text">About</div>
-          </a>
-        </Tippy>
-        <Tippy
-          content="Skills"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
-        >
-          <a href="#skills">
-            <TbPasswordUser />
-            <div className="text">Skills</div>
-          </a>
-        </Tippy>
-        <Tippy
-          content="Experience"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
-        >
-          <a href="#experience">
-            <BsSuitcaseLgFill />
-            <div className="text">Experience</div>
-          </a>
-        </Tippy>
-        <Tippy
-          content="Education"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
-        >
-          <a href="#education">
-            <RiGraduationCapFill />
-            <div className="text">Education</div>
-          </a>
-        </Tippy>
-        <Tippy
-          content="Projects"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
-        >
-          <a href="#projects">
-            <GoProjectRoadmap />
-            <div className="text">Projects</div>
-          </a>
-        </Tippy>
-        <Tippy
-          content="Achievements"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
-        >
-          <a href="#achievements">
-            <PiMedalFill />
-            <div className="text">Achievements</div>
-          </a>
-        </Tippy>
-        <Tippy
-          content="Contact"
-          className="tooltip"
-          animation="scale"
-          arrow={false}
-          hideOnClick={true}
-        >
-          <a href="#contact">
-            <FaPhone />
-            <div className="text">Contact</div>
-          </a>
-        </Tippy>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
-        </button>
+          JYCE.
+        </Link>
+        <div className="nav__menu show-menu" id="nav-menu">
+          <ul className="nav__list">
+            <li>
+              <Link
+                to="#"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="nav__link"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="#"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="nav__link"
+              >
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="#"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="nav__link"
+              >
+                My Info
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="#"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="nav__link"
+              >
+                Contact Me
+              </Link>
+            </li>
+          </ul>
+          <div className="nav__close" id="nav-close">
+            <i class="ri-close-large-line"></i>
+          </div>
+        </div>
+        <div className="nav__toggle" id="nav-toggle">
+          <i class="ri-menu-line"></i>
+        </div>
       </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
     </header>
   );
 }
