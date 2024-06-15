34f497773216981d571121e0790acd5ff7927c98
jaya2 <jaya2004kra@gmail.com>
2024-06-15 12:53:02 +0530

    added navbar

--- src/components/Header-Footer.js
+++ src/components/Header-Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import UpdateFooter from "./UpdateFooter";
+import Navbar from "./Navbar";
 
 function HeaderFooter() {
   return (
     <React.Fragment>
       <Sidebar />
       <UpdateFooter />
+      <Navbar />
     </React.Fragment>
   );
 }
