bc709e7936afebdab5f97c1a30f2dee886b565ca
jaya2 <jaya2004kra@gmail.com>
2024-06-15 22:37:23 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderFooter from "./components/Header-Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <React.Fragment>
       <HeaderFooter/>
+      <Home/>
     </React.Fragment>
   );
 }
