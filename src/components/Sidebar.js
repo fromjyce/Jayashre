e3a4760245ac5371fa1c8a96ad293213f2280eec
jaya2 <jaya2004kra@gmail.com>
2024-06-15 12:52:44 +0530

    added sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,38 +9,38 @@ function Sidebar() {
         <ul className="icons-list">
           <li className="icon one">
             <a className="icon-link" href="/#">
-              <i class="ri-discord-fill"></i>
+              <i class="ri-discord-fill side-icon"></i>
             </a>
           </li>
           <li className="icon two">
             <a className="icon-link" href="/#">
-              <i class="ri-graduation-cap-fill"></i>
+              <i class="ri-graduation-cap-fill side-icon"></i>
             </a>
           </li>
           <li className="icon three">
             <a className="icon-link" href="/#">
-              <i class="ri-github-fill"></i>
+              <i class="ri-github-fill side-icon"></i>
             </a>
           </li>
           <li className="icon four">
             <a className="icon-link" href="/#">
-              <i class="ri-linkedin-box-fill"></i>
+              <i class="ri-linkedin-box-fill side-icon"></i>
             </a>
           </li>
           <li className="icon five">
             <a className="icon-link" href="/#">
-              <i class="ri-mail-download-fill"></i>
+              <i class="ri-mail-download-fill side-icon"></i>
             </a>
           </li>
           <li className="icon six">
             <a className="icon-link" href="/#">
-              <i class="ri-mail-send-fill"></i>
+              <i class="ri-mail-send-fill side-icon"></i>
             </a>
           </li>
         </ul>
         <div className="settings-icon">
           <a className="icon-link" href="/#">
-            <i class="ri-settings-3-fill"></i>
+            <i class="ri-settings-3-fill side-icon"></i>
           </a>
         </div>
       </div>
