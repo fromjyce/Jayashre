f691d6b1a17d5e76baaefcc2d5d24fc2607fe37b
jaya2 <jaya2004kra@gmail.com>
2024-06-17 23:02:27 +0530

    added sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,47 +1,73 @@
 import React from "react";
 import "../styles/Sidebar.css";
 import "remixicon/fonts/remixicon.css";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
 
 function Sidebar() {
   return (
-    <div className="sidebar-main-container" id="side-bar">
-      <div className="actual-sidebar" id="actual-sidebar">
-        <ul className="icons-list">
-          <li className="icon one">
-            <a className="icon-link" href="/#">
-              <i class="ri-discord-fill side-icon"></i>
-            </a>
-          </li>
-          <li className="icon two">
-            <a className="icon-link" href="/#">
-              <i class="ri-graduation-cap-fill side-icon"></i>
-            </a>
-          </li>
-          <li className="icon three">
-            <a className="icon-link" href="/#">
-              <i class="ri-github-fill side-icon"></i>
-            </a>
-          </li>
-          <li className="icon four">
-            <a className="icon-link" href="/#">
-              <i class="ri-linkedin-box-fill side-icon"></i>
-            </a>
-          </li>
-          <li className="icon five">
+    <div className="sidebar-main-container" id="sidebar">
+      <div className="sidebar-container">
+        <div className="sidebar-item discord-one">
+          <a className="icon-link" href="/#">
+            <i class="ri-discord-fill side-icon"></i>
+          </a>
+        </div>
+        <div className="sidebar-item scholar-two">
+          <a className="icon-link" href="/#">
+            <i class="ri-graduation-cap-fill side-icon"></i>
+          </a>
+        </div>
+        <div className="sidebar-item github-three">
+          <a className="icon-link" href="/#">
+            <i class="ri-github-fill side-icon"></i>
+          </a>
+        </div>
+        <div className="sidebar-item linkedin-four">
+          <a className="icon-link" href="/#">
+            <i class="ri-linkedin-box-fill side-icon"></i>
+          </a>
+        </div>
+        <Tippy
+          content="Download Resume"
+          className="sidebar-tooltip"
+          animation="scale"
+          placement="right"
+          arrow={true}
+          hideOnClick={true}
+        >
+          <div className="sidebar-item resume-five">
             <a className="icon-link" href="/#">
               <i class="ri-mail-download-fill side-icon"></i>
             </a>
-          </li>
-          <li className="icon six">
+          </div>
+        </Tippy>
+        <Tippy
+          content="Send Mail"
+          className="sidebar-tooltip"
+          animation="scale"
+          placement="right"
+          arrow={true}
+          hideOnClick={true}
+        >
+          <div className="sidebar-item mail-six">
             <a className="icon-link" href="/#">
               <i class="ri-mail-send-fill side-icon"></i>
             </a>
-          </li>
-        </ul>
-        <div className="settings-icon">
-          <a className="icon-link" href="/#">
-            <i class="ri-settings-3-fill side-icon"></i>
-          </a>
+          </div>
+        </Tippy>
+        <div className="bottom-icons">
+          <div className="account-icon">
+            <a className="icon-link" href="/#">
+              <i class="ri-account-circle-fill side-icon"></i>
+            </a>
+          </div>
+          <div className="settings-icon">
+            <a className="icon-link" href="/#">
+              <i class="ri-settings-3-fill side-icon"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
