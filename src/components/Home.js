f0b91285f89b22a9effdfdc31f41bd64489db841
jaya2 <jaya2004kra@gmail.com>
2024-06-18 00:41:47 +0530

    added home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/Home.css";
 import Typewriter from "typewriter-effect";
+import { Link } from "react-scroll";
 
 function Home() {
   return (
@@ -12,12 +13,13 @@ function Home() {
           <Typewriter
             options={{
               strings: [
-                "a cybersecurity student.",
-                "a researcher.",
-                "a web developer.",
-                "an almost front-end developer.",
-                "a blockchain enthusiast.",
-                "open for work.",
+                "a Cybersecurity Student.",
+                "a Researcher.",
+                "a Web Developer.",
+                "an almost Front-End Developer.",
+                "a Blockchain Enthusiast.",
+                "a CTF Player.",
+                "Open for Work.",
               ],
               autoStart: true,
               loop: true,
@@ -25,7 +27,33 @@ function Home() {
           />
         </div>
         <div className="centered-buttons-container">
-
+          <div className="side-by-side-buttons-container">
+            <a className="home-link" href="/#">
+              <div className="button-one">
+                <i class="ri-github-fill button-icon"></i>
+                <div className="button-one-title">Follow! @fromjyce</div>
+              </div>
+            </a>
+            <a className="home-link" href="/#">
+              <div className="button-two">
+                <i class="ri-linkedin-box-fill button-icon"></i>
+                <div className="button-two-title">Connect with me!</div>
+              </div>
+            </a>
+          </div>
+          <Link
+            to="/#"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="home-link-item"
+          >
+            <div className="middle-button-container">
+              <i class="ri-telegram-2-fill button-icon"></i>
+              <div className="middle-button-title">Contact Me!</div>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
