dd3daeaa4d45522402c87056bdeab57ee493e923
jaya2 <jaya2004kra@gmail.com>
2024-06-17 23:23:02 +0530

    modified

--- src/components/Header-Footer.js
+++ src/components/Header-Footer.js
@@ -3,11 +3,12 @@ import Sidebar from "./Sidebar";
 import UpdateFooter from "./UpdateFooter";
 import Navbar from "./Navbar";
 
-function HeaderFooter() {
+function HeaderFooter({ children }) {
   return (
     <React.Fragment>
       <Navbar />
       <Sidebar />
+      <main className="content">{children}</main>
       <UpdateFooter />
     </React.Fragment>
   );
