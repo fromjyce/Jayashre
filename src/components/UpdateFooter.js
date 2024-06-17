2109464fe066fe44ef63f72eb9907dbea8b270d9
jaya2 <jaya2004kra@gmail.com>
2024-06-17 23:16:05 +0530

    added

--- src/components/UpdateFooter.js
+++ src/components/UpdateFooter.js
@@ -1,29 +1,18 @@
 import React from "react";
 import "../styles/UpdateFooter.css";
-import "remixicon/fonts/remixicon.css";
 
 function UpdateFooter() {
   return (
-    <div className="update-footer-main-container" id="update-footer">
-      <div className="update-footer-container">
-        <div className="github-container">
-          <ul className="github-list">
-            <li className="github-list-item branch-icon">
-              <i class="ri-git-branch-fill footer-icon"></i>
-            </li>
-            <li className="github-list-item branch-name">main</li>
-          </ul>
+    <div className="footer-main-container" id="footer">
+      <div className="footer-container">
+        <div className="left-icon">
+          <i class="ri-git-branch-fill"></i>
+          <span>main</span>
         </div>
-        <div className="main-container">
-          <div className="updation">Last Updated on July 2024</div>
-        </div>
-        <div className="smile-container">
-          <ul className="jyce-list">
-            <li className="jyce-list-item emoji">
-              <i class="ri-user-smile-fill footer-icon"></i>
-            </li>
-            <li className="jyce-list-item jyce-name">JYCE</li>
-          </ul>
+        <div className="center-text">Last Updated on June 2024</div>
+        <div className="right-icon">
+          <i class="ri-user-smile-fill"></i>
+          <span>JYCE</span>
         </div>
       </div>
     </div>
