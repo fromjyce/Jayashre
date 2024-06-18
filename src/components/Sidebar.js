92b5054cf2d18ed15d85f2cfbd8e3fdb3d906e9b
jaya2 <jaya2004kra@gmail.com>
2024-06-19 03:02:58 +0530

    added links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,28 +4,49 @@ import "remixicon/fonts/remixicon.css";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className="sidebar-main-container" id="sidebar">
       <div className="sidebar-container">
         <div className="sidebar-item discord-one">
-          <a className="icon-link" href="/#">
+          <a
+            className="icon-link"
+            href="https://discordapp.com/users/840610453431779348"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i class="ri-discord-fill side-icon"></i>
           </a>
         </div>
         <div className="sidebar-item scholar-two">
-          <a className="icon-link" href="/#">
+          <a
+            className="icon-link"
+            href="https://scholar.google.com/citations?user=PYXemgQAAAAJ&hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i class="ri-graduation-cap-fill side-icon"></i>
           </a>
         </div>
         <div className="sidebar-item github-three">
-          <a className="icon-link" href="/#">
+          <a
+            className="icon-link"
+            href="https://github.com/fromjyce"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i class="ri-github-fill side-icon"></i>
           </a>
         </div>
         <div className="sidebar-item linkedin-four">
-          <a className="icon-link" href="/#">
+          <a
+            className="icon-link"
+            href="https://www.linkedin.com/in/jayashrek/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i class="ri-linkedin-box-fill side-icon"></i>
           </a>
         </div>
@@ -38,7 +59,11 @@ function Sidebar() {
           hideOnClick={true}
         >
           <div className="sidebar-item resume-five">
-            <a className="icon-link" href="/#">
+            <a
+              className="icon-link"
+              href={require("../assets/resume.pdf")}
+              download
+            >
               <i class="ri-mail-download-fill side-icon"></i>
             </a>
           </div>
@@ -52,21 +77,26 @@ function Sidebar() {
           hideOnClick={true}
         >
           <div className="sidebar-item mail-six">
-            <a className="icon-link" href="/#">
+            <a
+              className="icon-link"
+              href="mailto:jaya2004kra@gmail.com"
+              target="_top"
+              rel="noopener noreferrer"
+            >
               <i class="ri-mail-send-fill side-icon"></i>
             </a>
           </div>
         </Tippy>
         <div className="bottom-icons">
           <div className="account-icon">
-            <a className="icon-link" href="/#">
+            <NavLink className="icon-link" to="/Jayashre">
               <i class="ri-account-circle-fill side-icon"></i>
-            </a>
+            </NavLink>
           </div>
           <div className="settings-icon">
-            <a className="icon-link" href="/#">
+            <NavLink className="icon-link" to="/contact">
               <i class="ri-settings-3-fill side-icon"></i>
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
