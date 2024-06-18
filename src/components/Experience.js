63ef97a1d59240e926259753c3a0a77997e6fc0c
jaya2 <jaya2004kra@gmail.com>
2024-06-18 22:35:37 +0530

    Added experience

---
+++ src/components/Experience.js
@@ -0,0 +1,322 @@
+import React from "react";
+import "remixicon/fonts/remixicon.css";
+import "../styles/Experience.css";
+
+function Experience () {
+    return (
+      <div className="experience-main-container">
+        <div className="experience-actual-container">
+          <h1 className="experience-title">experience</h1>
+          <div className="internship-experience-container">
+            <h2 className="internship-main-title">Internship Experience</h2>
+            <ul className="internship-list">
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Summer Research Fellow 2024
+                    </div>
+                  </div>
+                  <div className="internship-company">
+                    Indian Academy of Sciences, Bengaluru, Indian National
+                    Science Academy - INSA, New Delhi and The National Academy
+                    of Sciences - India, Allahabad
+                  </div>
+                  <div className="internship-timeline">May 2024 - Ongoing</div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Machine Learning,
+                    Malware Classification, Image Processing
+                  </div>
+                  <div className="internship-description">Ongoing</div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Educational Operations Coordinator
+                    </div>
+                  </div>
+                  <div className="internship-company">RAJ Online Tutoring</div>
+                  <div className="internship-timeline">
+                    April 2021 - Present
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Google Suite,
+                    Administration, Tutoring
+                  </div>
+                  <div className="internship-description">
+                    As the Assistant Tutor and Administrator at Raj Online
+                    Tutoring, I serve as the Substitute Tutor when the main
+                    tutor is unavailable. In addition to conducting tutoring
+                    sessions, my responsibilities extend to managing meeting
+                    links, overseeing the financial aspects of weekly fees,
+                    preparing assignments and question papers, and addressing
+                    any software or technical issues that may arise during
+                    classes.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Web Development Intern
+                    </div>
+                    <a
+                      className="experience-link-icon"
+                      href="/#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i class="ri-github-fill experience-icon"></i>
+                    </a>
+                  </div>
+                  <div className="internship-company">Info Aid Tech</div>
+                  <div className="internship-timeline">
+                    September 2023 - October 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Web Development, HTML,
+                    CSS, JavaScript, Django, MongoDB, MySQL, ReactJS, Node JS,
+                    Flask, API, Figma
+                  </div>
+                  <div className="internship-description">
+                    Throughout my immersive internship at InfoAidTech, I engaged
+                    in a series of impactful web development projects that
+                    underscored my growth as a developer. Starting with a
+                    captivating product landing page for premium watches, I
+                    honed my skills in crafting sleek and user-friendly
+                    interfaces. The development of a functional calculator then
+                    challenged me to handle a range of arithmetic operations and
+                    more complex calculations. Diving into practical utility, I
+                    created a temperature converter, streamlining the process of
+                    unit conversions. The culmination of my internship involved
+                    the creation of an enticing restaurant website, where I
+                    combined aesthetics with functionality.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Java Programming Intern
+                    </div>
+                    <a
+                      className="experience-link-icon"
+                      href="/#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i class="ri-github-fill experience-icon"></i>
+                    </a>
+                  </div>
+                  <div className="internship-company">CodSoft</div>
+                  <div className="internship-timeline">
+                    August 2023 - September 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Java, API, OOPs,
+                    JavaFX-GUI
+                  </div>
+                  <div className="internship-description">
+                    Throughout my Java programming internship at CodSoft, I
+                    underwent a significant evolution, transitioning from an
+                    intern to a skilled Java programmer. The projects I
+                    undertook during this period speak to the diverse range of
+                    skills I acquired. From creating a Number Game that involved
+                    number generation and user interaction to developing The
+                    Student Grade Calculator for grade calculations and
+                    implementing a user-friendly ATM Interface with transaction
+                    handling, each project contributed to my growth. The
+                    Real-Time Currency Converter demonstrated my ability to
+                    integrate real-time data using external APIs, while the
+                    Student Management System showcased my proficiency in
+                    designing user-friendly interfaces for efficient data
+                    management.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Web Development & Design Intern
+                    </div>
+                    <a
+                      className="experience-link-icon"
+                      href="/#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i class="ri-github-fill experience-icon"></i>
+                    </a>
+                  </div>
+                  <div className="internship-company">Oasis Infobyte</div>
+                  <div className="internship-timeline">
+                    August 2023 - September 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Web Development, UX,
+                    HTML, CSS, JS, API, React, Node.js, MongoDB, Flask
+                  </div>
+                  <div className="internship-description">
+                    During my immersive internship at Oasis Infobyte, I
+                    progressed through various web development tasks, starting
+                    with personal portfolios and landing pages and advancing to
+                    building tools like temperature converters and calculators
+                    using HTML, CSS, and JavaScript. I contributed to projects
+                    such as a tribute page and a to-do web app, emphasizing both
+                    aesthetics and usability. Moving into advanced development,
+                    I tackled Flask for login authentication and embraced
+                    full-stack development with React, MongoDB, and Node.js to
+                    create a sophisticated Pizza Delivery Application. This
+                    project showcased my skills in user and admin
+                    authentication, dynamic features like pizza customization,
+                    secure payments via Razor Pay, and efficient inventory
+                    management.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Web Development Intern
+                    </div>
+                    <a
+                      className="experience-link-icon"
+                      href="/#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i class="ri-github-fill experience-icon"></i>
+                    </a>
+                  </div>
+                  <div className="internship-company">Code Alpha</div>
+                  <div className="internship-timeline">
+                    August 2023 - September 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Web Development, UX,
+                    HTML, CSS, JS, PHP
+                  </div>
+                  <div className="internship-description">
+                    In my role as a web development intern at Code Alpha, I
+                    meticulously documented my learning journey through a series
+                    of projects showcased in this GitHub repository. Beginning
+                    with the creation of a versatile Multi Survey Form using
+                    JavaScript, HTML, and CSS with PHP integration for seamless
+                    survey response delivery, I demonstrated my evolving skills.
+                    Strengthening my commitment to security, I further
+                    integrated Google's reCAPTCHA service into the survey form.
+                    Transitioning into web design, I showcased my layout and
+                    design proficiency by creating a clone of the Netflix
+                    webpage.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Content Writing Intern
+                    </div>
+                  </div>
+                  <div className="internship-company">CoConnexion</div>
+                  <div className="internship-timeline">
+                    July 2023 - September 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Technical Writing,
+                    Written Communication, Teamwork
+                  </div>
+                  <div className="internship-description">
+                    Developed proficiency as a technical content writer,
+                    crafting insightful blogs and articles on emerging
+                    technologies. Shared knowledge through publications on
+                    LinkedIn and Medium to contribute to the dissemination of
+                    industry insights and expertise.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Summer Intern - Website Committee
+                    </div>
+                  </div>
+                  <div className="internship-company">
+                    World Affairs Council of Harrisburg
+                  </div>
+                  <div className="internship-timeline">
+                    June 2023 - August 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Web Development, Public
+                    Speaking, Presentation, Wordpress
+                  </div>
+                  <div className="internship-description">
+                    During my role, I played a key part in updating and
+                    maintaining the WACH website and elevating the WACH
+                    newsletter by introducing a monthly intern column, enhancing
+                    its appeal and engagement. Additionally, I delivered a
+                    presentation on mental health in China and India,
+                    complemented by an op-ed on the same topic. Furthermore, I
+                    contributed to the "2023 International Young Leaders
+                    Conference" by preparing comprehensive questionnaires for
+                    speakers.
+                  </div>
+                </div>
+              </li>
+              <li className="internship-list-item">
+                <div className="internship-info-container">
+                  <div className="internship-name-icon-container">
+                    <div className="internship-name">
+                      Python Programming Intern
+                    </div>
+                    <a
+                      className="experience-link-icon"
+                      href="/#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i class="ri-github-fill experience-icon"></i>
+                    </a>
+                  </div>
+                  <div className="internship-company">InternPe</div>
+                  <div className="internship-timeline">
+                    May 2023 - June 2023
+                  </div>
+                  <div className="stack-skills">
+                    <strong>Skills & Concepts: </strong>Python, OOPs, CNN,
+                    Project Management, Tkinter-GUI
+                  </div>
+                  <div className="internship-description">
+                    At InternPe, I mastered Object-Oriented Programming,
+                    creating organized and reusable Python code. I optimized
+                    program efficiency with Python modules and demonstrated
+                    precision in data manipulation. The internship featured
+                    three projects: a Countdown Timer script, a Tkinter-based
+                    GUI app for dice and coin actions, and SmileSnapWaveStop,
+                    utilizing OpenCV for smile detection and hand tracking in
+                    automatic screenshot capture.
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="mentee-experience-container">
+            <h2 className="mentee-main-title">Mentee Experience</h2>
+          </div>
+          <div className="virtual-experience-container">
+            <h2 className="virtual-main-title">Virtual Experience</h2>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Experience;
