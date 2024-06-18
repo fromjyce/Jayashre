3a2fef779fa98ac27f5df8e344a5c31f1926794d
jaya2 <jaya2004kra@gmail.com>
2024-06-18 10:42:07 +0530

    added skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -22,6 +22,12 @@ function Skills() {
               <li className="technical-skills-list-item">
                 Machine Learning Model Development
               </li>
+              <li className="technical-skills-list-item">
+                Real Time Data Preprocessing
+              </li>
+              <li className="technical-skills-list-item">
+                Technical Writing / Documentation
+              </li>
             </ul>
           </div>
           <div className="program-langs-container">
@@ -32,9 +38,12 @@ function Skills() {
               <li className="program-langs-list-item">C</li>
               <li className="program-langs-list-item">Python</li>
               <li className="program-langs-list-item">SQL</li>
+              <li className="program-langs-list-item">MySQL</li>
+              <li className="program-langs-list-item">MongoDB</li>
               <li className="program-langs-list-item">Scala</li>
               <li className="program-langs-list-item">Git / GitHub / GitLab</li>
               <li className="program-langs-list-item">Haskell</li>
+              <li className="program-langs-list-item">Markdown</li>
               <li className="program-langs-list-item">Latex</li>
               <li className="program-langs-list-item">Sphinx</li>
               <li className="program-langs-list-item">Java</li>
@@ -45,12 +54,74 @@ function Skills() {
               <li className="program-langs-list-item">Bash/Shell Scripting</li>
               <li className="program-langs-list-item">HTML</li>
               <li className="program-langs-list-item">CSS</li>
+              <li className="program-langs-list-item">Sass</li>
               <li className="program-langs-list-item">Javascript</li>
               <li className="program-langs-list-item">Flask</li>
               <li className="program-langs-list-item">React JS</li>
+              <li className="program-langs-list-item">Node JS</li>
+              <li className="program-langs-list-item">PostgreSQL</li>
               <li className="program-langs-list-item">Next JS</li>
               <li className="program-langs-list-item">Electron JS</li>
               <li className="program-langs-list-item">Vite JS</li>
+            </ul>
+          </div>
+          <div className="products-container">
+            <h2 className="products-title">Products</h2>
+            <ul className="products-list">
+              <li className="products-list-item">Windows</li>
+              <li className="products-list-item">Kali Linux</li>
+              <li className="products-list-item">Ubuntu</li>
+              <li className="products-list-item">Netlify</li>
+              <li className="products-list-item">Microsoft Suite</li>
+              <li className="products-list-item">Android Studio</li>
+              <li className="products-list-item">Visual Studio Code</li>
+              <li className="products-list-item">PyCharm</li>
+              <li className="products-list-item">Anaconda</li>
+              <li className="products-list-item">Eclipse</li>
+              <li className="products-list-item">Figma</li>
+              <li className="products-list-item">Wireshark</li>
+              <li className="products-list-item">CodePen</li>
+              <li className="products-list-item">Wordpress</li>
+              <li className="products-list-item">Burpsuite</li>
+              <li className="products-list-item">Nmap</li>
+              <li className="products-list-item">Vercel</li>
+              <li className="products-list-item">Nessus</li>
+              <li className="products-list-item">Google Cloud Platform</li>
+              <li className="products-list-item">Docker</li>
+            </ul>
+          </div>
+          <div className="interpersonal-skills-container">
+            <h2 className="interpersonal-skills-title">Interpersonal Skills</h2>
+            <ul className="interpersonal-skills-list">
+              <li className="interpersonal-skills-list-item">
+                Time Management
+              </li>
+              <li className="interpersonal-skills-list-item">
+                Project Management
+              </li>
+              <li className="interpersonal-skills-list-item">
+                Problem Solving
+              </li>
+              <li className="interpersonal-skills-list-item">Leadership</li>
+              <li className="interpersonal-skills-list-item">Teamwork</li>
+              <li className="interpersonal-skills-list-item">
+                Public Speaking
+              </li>
+              <li className="interpersonal-skills-list-item">
+                Technical Writing
+              </li>
+              <li className="interpersonal-skills-list-item">
+                Flexibility & Adaptability
+              </li>
+            </ul>
+          </div>
+          <div className="lang-skills-container">
+            <h2 className="lang-skills-title">Language Proficiency</h2>
+            <ul className="lang-skills-list">
+              <li className="langs-skills-list-item">Tamil - Native</li>
+              <li className="langs-skills-list-item">English - Native</li>
+              <li className="langs-skills-list-item">Hindi - Intermediate</li>
+              <li className="langs-skills-list-item">German - A1 - Beginner</li>
             </ul>
           </div>
         </div>
