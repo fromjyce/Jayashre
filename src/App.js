4fdb50f247bc83c4625a1ee870ec1e0d0e10988a
jaya2 <jaya2004kra@gmail.com>
2024-06-19 01:57:05 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderFooter from "./components/Header-Footer";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
-import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Achievements from "./components/Achievements";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <HeaderFooter>
-        <Achievements />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route index path="/Jayashre" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </HeaderFooter>
-    </React.Fragment>
+    </BrowserRouter>
   );
 }
 
