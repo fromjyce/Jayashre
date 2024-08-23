60baf9bbeee8809d6828409ce4ce362eb148bfa4
Jayashre <jaya2004kra@gmail.com>
2024-08-24 00:22:12 +0530

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,21 +7,30 @@ const projects = [
   {
     title: "Personal Portfolio Website",
     description:
-      "Designed a dynamic personal portfolio website showcasing my professional achievements, skills, experiences, achievements, and notable projects.",
+      "Created a dynamic portfolio website to highlight professional achievements, skills, and projects using React JS, HTML, CSS, and JavaScript, designed for a compelling online presence.",
     link: "https://github.com/fromjyce/Jayashre",
     contributor: "Solo Contributor",
     timeline: "Ongoing - Managed by me" /*January */,
-    stack: "React JS, HTML, CSS, Javascript, Figma, UX",
+    stack: "React JS, HTML/CSS, Javascript, Figma, UX",
   },
+  {
+    title: "WebShield",
+    description:
+      "Developed a high-accuracy phishing detection solution using a Chrome extension, ReactJS, FastAPI, and Firebase, achieving 98% accuracy across five machine learning models and enhancing online security.",
+    link: "https://github.com/fromjyce/WebShield",
+    contributor: "Solo Contributor",
+    timeline: "July 2024 - August 2024" /*January */,
+    stack: "Python, Fast API, HTML/CSS, JavaScript, ReactJS, Firebase, Machine Learning, Phishing Detection",
+  },/*
   {
     title: "CipherGuard: A Secure Encryption/Decryption Tool",
     description:
       "SwingFX Java application for text, document, audio, video, and image encryption.",
     link: "https://github.com/fromjyce/CipherGuard",
     contributor: "Solo Contributor",
-    timeline: "Ongoing" /*December */,
+    timeline: "Ongoing" /*December */,/*
     stack: "Java, SwingFX, GUI, Cryptography",
-  } /*
+  }, /*
   {
     title: "BlockSecure",
     description:
@@ -31,32 +40,31 @@ const projects = [
     timeline: "Ongoing" /*January */ /*
     stack:
       "Next JS, ViteJS, ReactJS, HTML, CSS, JavaScript, Solidity, Ethereum, Figma",
-  },*/,
-  ,
+  },*/
   {
     title: "Visual Based Malware Classification",
     description:
-      "Developed a visual-based malware classification system leveraging image processing and machine learning techniques to enhance detection accuracy and efficiency.",
+      "Contributed to research on visual-based malware classification under Dr. Pavan Kumar C, enhancing detection accuracy by 98%. Implemented advanced image processing and machine learning techniques to significantly improve the efficiency of malware detection systems.",
     link: "https://github.com/fromjyce/Visual-Based-Malware-Classification",
     contributor: "Solo Contributor",
     timeline: "May 2024 - July 2024",
     stack:
-      "Python, Deep Learning, Machine Learning, Image Processing, Feature Extraction, Malware Analysis",
+      "Python, Deep Learning, Machine Learning, Image Processing, Feature Extraction, Malware Analysis, Data Engineering",
   },
   {
     title: "SecureDash",
     description:
-      "A real-time monitoring dashboard leveraging machine learning to enhance Smart Grid security against cyber threats.",
+      "Engineered a real-time monitoring dashboard to boost Smart Grid security by 90% through advanced machine learning, utilizing ElectronJS, Flask, PowerBI, and various datasets.",
     link: "https://github.com/fromjyce/SecureDash",
     contributor: "Solo Contributor",
     timeline: "March 2024 - May 2024",
     stack:
-      "Electron JS, HTML, CSS, Javascript, Scapy, Flask, Python, MySQL, PowerBI, CICDDOS2019 Dataset, Machine Learning, Deep Learning",
+      "Electron JS, HTML/CSS, Javascript, Scapy, Flask, Python, MySQL, PowerBI, CICDDOS2019 Dataset, Machine Learning, Deep Learning",
   },
   {
     title: "BotDetect",
     description:
-      "A graph-based ML model for botnet detection utilizing feature selection to enhance detection rates, reduce complexity, and improve robustness against zero-day attacks.",
+      "Designed a graph-based machine learning model for botnet detection, improving detection rates and system robustness by 95% against zero-day attacks, leveraging Python and advanced data processing techniques.",
     link: "https://github.com/fromjyce/BotDetect",
     contributor: "Team Contributor",
     timeline: "March 2024 - April 2024",
@@ -66,7 +74,7 @@ const projects = [
   {
     title: "Diamond Bidding Game",
     description:
-      "A graphical card game using Pygame, where players compete against the computer by selecting and bidding cards to achieve the highest score.",
+      "Created a graphical card game with Pygame where players bid and select cards to score highest, showcasing skills in game development and algorithms.",
     link: "https://github.com/fromjyce/WE-Module3/tree/main/GenAI-Assignment-6",
     contributor: "Solo Contributor",
     timeline: "March 2024",
@@ -75,16 +83,16 @@ const projects = [
   {
     title: "Algorithmic Trading Model Development",
     description:
-      "Development of an Algorithmic Trading Model for the BTC/USDT Crypto Market",
+      "Developed a model for algorithmic trading in the BTC/USDT crypto market, applying machine learning and deep learning techniques for trading strategy optimization.",
     link: "https://github.com/fromjyce/Algorithmic-Trading",
     contributor: "Team Contributor",
-    timeline: "January 2024" /*January */,
+    timeline: "January 2024",
     stack: "Python, Machine Learning, Trading, Deep Learning",
   },
   {
     title: "HireMeHorizon: Smart Campus Recruiter",
     description:
-      "Applying machine learning to campus placement data involves analyzing student info, using models, and developing a website for undergraduates to assess placement and predict salaries.",
+      "Created a predictive website to improve campus placement outcomes by 94% using machine learning for accurate salary predictions, integrating Python, ReactJS, and Flask.",
     link: "https://github.com/fromjyce/HireMeHorizon",
     contributor: "Team Contributor",
     timeline: "October 2023 - December 2023",
@@ -94,16 +102,16 @@ const projects = [
   {
     title: "Deepfake Detection with InceptionResNetV2-BiLSTM",
     description:
-      "This project enhances deepfake detection by combining InceptionResNetV2 and BiLSTM networks, addressing concerns about deceptive content in multimedia.",
+      "Developed a deepfake detection system integrating InceptionResNetV2 with BiLSTM networks, achieving over 94% accuracy in identifying deepfake content. Utilized Python and TensorFlow-Keras to build a robust solution for multimedia content verification on a dedicated website.",
     link: "https://github.com/fromjyce/DeepfakeDetection",
     contributor: "Solo Contributor",
     timeline: "October 2023 - December 2023",
-    stack: "Python, Deep Learning, Tensorflow-Keras, Flask, Numpy, OpenCV",
+    stack: "Python, Deep Learning, Tensorflow-Keras, Flask, Numpy, OpenCV, HTML/CSS, JavaScript, ReactJS",
   },
   {
     title: "PenGUIn",
     description:
-      "Developed a user-friendly Linux app for OS hardening with essential and advanced security options, one-click USB and Tor exit node blocking, Essential and Advanced modes, and additional features like cache cleaning for heightened system security.",
+      "Co-developed a user-friendly Linux application for OS hardening, offering essential and advanced security features with streamlined one-click options, using PyGTK and Shell scripting.",
     link: "https://github.com/fromjyce/PenGUIn",
     contributor: "Team Contributor",
     timeline: "October 2023 - December 2023",
@@ -112,7 +120,7 @@ const projects = [
   {
     title: "AirPic",
     description:
-      "A collaborative project revolutionizing camera interaction. Introduced smile detection, hand gesture-controlled capture, zoom via gestures, and dynamic filters.",
+      "Led the development of a gesture-controlled app with Kotlin and TensorFlow-Keras, featuring advanced gesture recognition for an enhanced user experience.",
     link: "https://github.com/fromjyce/AirPic",
     contributor: "Team Contributor",
     timeline: "June 2023 - November 2023",
@@ -122,12 +130,12 @@ const projects = [
   {
     title: "OpenChain",
     description:
-      "A Solidity smart contract transforming the digital content marketplace, was developed using ThirdWeb.",
+      "Designed a Solidity smart contract for a digital content marketplace, utilizing Remix IDE and ThirdWeb to innovate blockchain applications.",
     link: "https://github.com/fromjyce/OpenChain",
     contributor: "Solo Contributor",
     timeline: "October 2023 - November 2023",
     stack: "Solidity, Remix IDE, Thirdweb, Blockchain, Ethereum",
-  },
+  },/*
   {
     title: "Student Management System",
     description:
@@ -154,11 +162,11 @@ const projects = [
     contributor: "Solo Contributor",
     timeline: "August 2023 - September 2023",
     stack: "HTML, Figma, Javascript, CSS, UX",
-  },
+  },*/
   {
     title: "CameraX",
     description:
-      "An innovative camera app coded in Kotlin, leveraging the power of CameraX library for a seamless photography experience.",
+      "Developed a camera application using Kotlin and CameraX for a seamless photography experience with enhanced features.",
     link: "https://github.com/fromjyce/CameraX",
     contributor: "Solo Contributor",
     timeline: "July 2023 - August 2023",
@@ -167,7 +175,7 @@ const projects = [
   {
     title: "American Sign Language Detection",
     description:
-      "An ASL Gesture Recognition System in Python using CNNs for Improved Accessibility and Inclusivity.",
+      "Created an ASL gesture recognition system using CNNs, achieving 90% accuracy to enhance accessibility. Utilized Python and TensorFlow-Keras for effective gesture recognition.",
     link: "https://github.com/fromjyce/AmericanSignLanguageDetection",
     contributor: "Team Contributor",
     timeline: "March 2023 - May 2023",
@@ -176,7 +184,7 @@ const projects = [
   {
     title: "Barcode Based Attendance System",
     description:
-      "Developed a Python-based attendance system using barcode scanning and Excel files for user-friendly database management.",
+      "Designed an attendance system using barcode scanning and Python, integrating Excel for user-friendly database management.",
     link: "https://github.com/fromjyce/BarCodeAttendanceSystem",
     contributor: "Solo Contributor",
     timeline: "March 2023 - May 2023",
@@ -185,7 +193,7 @@ const projects = [
   {
     title: "Smile Snap Wave Stop",
     description:
-      "The project involves smile detection and screenshot capture using OpenCV, along with hand detection to terminate the program.",
+      "Implemented a project for smile detection and screenshot capture, using OpenCV and hand detection for program control.",
     link: "https://github.com/fromjyce/SmileSnapWaveStop",
     contributor: "Solo Contributor",
     timeline: "May 2023 - June 2023",
@@ -194,7 +202,7 @@ const projects = [
   {
     title: "Emotions based Music Player",
     description:
-      "A first-semester mini project's C program played songs based on user-input emotions using a simple interface and external libraries.",
+      "Developed a C-based music player that selects songs based on user-input emotions, utilizing a simple interface and external libraries.",
     link: "https://github.com/fromjyce/SongPlayer-In-C",
     contributor: "Solo Contributor",
     timeline: "October 2022 - December 2022",
