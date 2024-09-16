c0b305c560cefbfcba4ef1e0b1b5642e2ba4427c
Jayashre <jaya2004kra@gmail.com>
2024-09-16 14:13:15 +0530

    changed resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,11 +4,11 @@ function Resume() {
   return (
     <div style={{ margin: 0, padding: 0, height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src={`${process.env.PUBLIC_URL}/resume.pdf`}
+        src={`${process.env.PUBLIC_URL}/display-resume.pdf`}
         style={{ width: '100%', height: '100vh', border: 'none' }}
         title="Resume PDF"
       >
-        This browser does not support PDFs. Please download the PDF to view it: <a href={`${process.env.PUBLIC_URL}/resume.pdf`}>Download PDF</a>.
+        This browser does not support PDFs. Please download the PDF to view it: <a href={`${process.env.PUBLIC_URL}/display-resume.pdf`}>Download PDF</a>.
       </iframe>
     </div>
   );
