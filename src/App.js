aa243742f59e5658d3c027c71b4d3c2f1aa4538f
Jayashre <jaya2004kra@gmail.com>
2024-09-16 14:01:58 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import HeaderFooter from "./components/Header-Footer";
-import Home from "./components/Home";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import Achievements from "./components/Achievements";
-import Contact from "./components/Contact";
+import React from 'react';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import RouteWrapper from './components/RouteWrapper';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Achievements from './components/Achievements';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <HashRouter>
-      <HeaderFooter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route element={<RouteWrapper />}>
           <Route path="/home" element={<Home />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
@@ -22,8 +23,9 @@ function App() {
           <Route path="/experience" element={<Experience />} />
           <Route path="/achievements" element={<Achievements />} />
           <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </HeaderFooter>
+        </Route>
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
     </HashRouter>
   );
 }
