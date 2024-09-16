52052e198eadb407ecf92076b9997156deff43c0
Jayashre <jaya2004kra@gmail.com>
2024-09-16 14:00:24 +0530

    added inline style

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import "../styles/Resume.css";
 
 function Resume() {
   return (
-    <div className='main'>
+    <div style={{ margin: 0, padding: 0, height: '100vh', overflow: 'hidden' }}>
       <iframe
         src={`${process.env.PUBLIC_URL}/resume.pdf`}
         style={{ width: '100%', height: '100vh', border: 'none' }}
