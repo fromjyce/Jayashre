036c871113593f34f3146b1b9fff1533df8e75a5
Jayashre <jaya2004kra@gmail.com>
2024-10-31 13:29:02 +0530

    added resume paths - primary

--- src/components/DSResume.js
+++ src/components/DSResume.js
@@ -4,7 +4,7 @@ function DSResume() {
   return (
     <div style={{ margin: 0, padding: 0, height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src={`${process.env.PUBLIC_URL}/display-resume.pdf`}
+        src={`${process.env.PUBLIC_URL}/resumes/ds-resume.pdf`}
         style={{ width: '100%', height: '100vh', border: 'none' }}
         title="Resume PDF"
       >
--- src/components/SDEResume.js
+++ src/components/SDEResume.js
@@ -4,7 +4,7 @@ function SDEResume() {
   return (
     <div style={{ margin: 0, padding: 0, height: '100vh', overflow: 'hidden' }}>
       <iframe
-        src={`${process.env.PUBLIC_URL}/display-resume.pdf`}
+        src={`${process.env.PUBLIC_URL}/resumes/sde-resume.pdf`}
         style={{ width: '100%', height: '100vh', border: 'none' }}
         title="Resume PDF"
       >
