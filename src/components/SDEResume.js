b8af4e0283a608fd9938e23fe7f726ea86557300
Jayashre <jaya2004kra@gmail.com>
2024-10-31 13:14:06 +0530

    added code

---
+++ src/components/SDEResume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function SDEResume() {
+  return (
+    <div style={{ margin: 0, padding: 0, height: '100vh', overflow: 'hidden' }}>
+      <iframe
+        src={`${process.env.PUBLIC_URL}/display-resume.pdf`}
+        style={{ width: '100%', height: '100vh', border: 'none' }}
+        title="Resume PDF"
+      >
+        This browser does not support PDFs. Please download the PDF to view it: <a href={`${process.env.PUBLIC_URL}/display-resume.pdf`}>Download PDF</a>.
+      </iframe>
+    </div>
+  );
+}
+
+export default SDEResume;
